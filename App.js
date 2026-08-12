63f7775d448b78099d2e1dcfe3c6cbe319c1c868
Fabio Cirilo <cirilofabio84@gmail.com>
2026-08-12 11:27:50 +0100

    Consuming an API with Axios

--- App.js
+++ App.js
@@ -1,8 +1,41 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
 import PickerItem from './src/components/picker'
+import { useEffect, useState } from 'react'
+import api from './src/services/api'
 
 export default function App() {
+  const [moedas, setMoedas] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function loadMoedas() {
+      const response = await api.get('all')
+      //console.log(response.data)
+
+      let arrayMoedas = []
+      Object.keys(response.data).map(key => {
+        arrayMoedas.push({
+          key: key,
+          label: key,
+          value: key
+        })
+      })
+
+      //console.log(arrayMoedas)
+      setMoedas(arrayMoedas)
+      setLoading(false)
+    }
+
+    loadMoedas()
+  }, [])
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator color="#fff" size="large" />
+      </View>
+    )
+  }
   return (
     <View style={styles.container}>
       <View style={styles.areaMoeda}>
